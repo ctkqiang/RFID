2c00a2d08d735ed325cb319d7a6f8c62e9f73971
John Melody Me <Johnmelodyme@yandex.com>
2021-05-15 20:22:40 +0800

    Update RFID.ino

--- RFID/RFID.ino
+++ RFID/RFID.ino
@@ -1,37 +1,38 @@
 /**
- * @author: John Melody Me
- * @contact: <JOhnmelody@dingtalk.com>
- */
+   @author: John Melody Me
+   @contact: <JOhnmelody@dingtalk.com>
+*/
 #include "SPI.h"
 #include "MFRC522.h"
 
 /**
- * +-----------------+-------------+
- * | RFID RC522 PIN  | ARDUINO UNO |
- * +-----------------+-------------+
- * | SDA             | 10          |
- * | SCK             | 13          |
- * | MOSI            | 11          |
- * | MISO            | 12          |
- * | IRQ             | UNUSED      |
- * | GND             | GND         |
- * | RST             | 9           |
- * | 3.3V            | 3.3V        |
- * +-----------------+-------------+
- */
- 
+   +-----------------+-------------+
+   | RFID RC522 PIN  | ARDUINO UNO |
+   +-----------------+-------------+
+   | SDA             | 10          |
+   | SCK             | 13          |
+   | MOSI            | 11          |
+   | MISO            | 12          |
+   | IRQ             | UNUSED      |
+   | GND             | GND         |
+   | RST             | 9           |
+   | 3.3V            | 3.3V        |
+   +-----------------+-------------+
+*/
+
 #define PIN_RST 9
 #define PIN_SDA 10
-
-const int rfidTone = 50;
-const int duration = 30;
-
-int piezoPin = 8;
+#define BAUD_RATE 9600
+#define RFID_TONE 1000
+#define BUZZER 3
+#define DURATION 500
+#define PIEZO_PIN 8
 
 int tones[] = { 1915, 1700, 1519, 1432, 1275, 1136, 1014, 956};
 
 String tagID = "";
 String MASTER_TAG = "D743AF7B";
+String MASTER_TAG_ALT = "5527112A";
 
 MFRC522 mfrc522(PIN_SDA, PIN_RST);
 
@@ -45,12 +46,12 @@ void setup()
   mfrc522.PCD_DumpVersionToSerial();
 
   // Initiate Serial Connection
-  Serial.begin(9600);
+  Serial.begin(BAUD_RATE);
   while (!Serial);
 
   if (!mfrc522.PICC_IsNewCardPresent())
   {
-    Serial.println(" No Card Presented ");
+    Serial.println(" No Card Presented.");
   }
 }
 
@@ -62,19 +63,17 @@ void loop()
     // if RFID tag/card was read return TRUE
     if (mfrc522.PICC_ReadCardSerial())
     {
-      Serial.print("RFID TAG ID -> ");
+      // Serial.print("RFID TAG ID -> ");
 
-      playTone(rfidTone, duration);
+      playTone(RFID_TONE, DURATION);
 
       for (byte i = 0; i < mfrc522.uid.size; ++i)
       {
-        Serial.print(mfrc522.uid.uidByte[i], HEX);
-        Serial.print(" ");
+        // Serial.print(mfrc522.uid.uidByte[i], HEX);
+        // Serial.print(" ");
 
         verification();
       }
-
-      Serial.println();
     }
   }
 
@@ -82,25 +81,19 @@ void loop()
   // mfrc522.PICC_DumpToSerial(&(mfrc522.uid));
 }
 
-void playTone(int tone, int duration)
+void playTone(int rfidTone, int duration)
 {
-  for (long i = 0; i < duration * 1000L; i += tone * 2)
-  {
-    digitalWrite(piezoPin, HIGH);
-    delayMicroseconds(tone);
-    digitalWrite(piezoPin, LOW);
-    delayMicroseconds(tone);
-  }
+  tone(PIEZO_PIN , rfidTone, duration);
 }
 
 boolean getCardIdentification()
 {
-  if (! mfrc522.PICC_IsNewCardPresent())
+  if (!mfrc522.PICC_IsNewCardPresent())
   {
     return false;
   }
 
-  if ( ! mfrc522.PICC_ReadCardSerial())
+  if (!mfrc522.PICC_ReadCardSerial())
   {
     //Since a PICC placed get Serial and continue
     return false;
@@ -108,7 +101,7 @@ boolean getCardIdentification()
 
   tagID = "";
 
-  for ( uint8_t i = 0; i < 4; i++)
+  for (uint8_t i = 0; i < 4; i++)
   {
     // The MIFARE PICCs that we use have 4 byte UID
     tagID.concat(String(mfrc522.uid.uidByte[i], HEX)); // Adds the 4 bytes in a single String variable
@@ -126,11 +119,13 @@ void verification()
   {
     if (tagID == MASTER_TAG)
     {
-      log("Granted");
+      log("Access Granted");
+      log("Card ID: " + tagID + "\n");
     }
     else
     {
-      log("Denied");
+      log("Access Denied");
+      log("Card ID: " + tagID + "\n");
     }
   }
 }
