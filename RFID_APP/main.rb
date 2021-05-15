d5405ca33791a0b7772833a4bb8602a83b00c36d
John Melody Me <Johnmelodyme@yandex.com>
2021-05-16 01:25:23 +0800

    Update main.rb

--- RFID_APP/main.rb
+++ RFID_APP/main.rb
@@ -1,5 +1,23 @@
-require 'serialport'
-
-ser = SerialPort.new("COM14", 9600, 8, 1, SerialPort::NONE)
-
-puts ser.readlines
+class ArduinoService  
+    def initialize
+        port_str = "COM14"
+        baud_rate = 9600
+        data_bits = 8
+        stop_bits = 1
+        parity = SerialPort::NONE 
+        
+        if port_connected?(port_str)
+            sp = SerialPort.new(port_str, baud_rate, data_bits, stop_bits, parity)
+            order_confirmation(sp)
+        end
+    end  
+    
+    def order_confirmation(sp)
+        sp.write('a')
+        sp.flush
+    end  
+    
+    def port_connected?(port)
+        return true if Dir.glob(port).count == 1
+    end
+end
